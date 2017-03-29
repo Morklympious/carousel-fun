53cb06629d3b31f514865a7ae2df0c2819b9267f
Bradley Stafford <Morklympious@gmail.com>
2017-03-29 12:44:46 -0700

    clean up code a little bit

--- src/js/event-logic/carousel-event-listeners.js
+++ src/js/event-logic/carousel-event-listeners.js
@@ -9,57 +9,51 @@ function listenOn(element, event, fn) {
   return element.addEventListener(event, fn);
 }
 
-function bounded(value, lower, upper) {
-  return (lower <= value && value <= upper);
-}
-
-function ondragstart(Carousel, root, state) {
-  listenOn(root, events.start, function(e) {
-      var pos = coordinates(e);
-
-      /** Beginning a drag */
-      state.dragging = true;
-      state.position.start = pos.x;
-      console.log(state);
-  });
-}
-
-function ondragmove(Carousel, root, state) {
-  var slides = Carousel.dom.wrapper;
-
-  listenOn(root, events.move, function(e) {
-    var pos = coordinates(e),
-        newPosition = state.position.offset + state.position.moved;
-
-    /** If we're not flagged as dragging, do nothing */
-    if(!state.dragging) return;
-
-    state.position.moved = pos.x - state.position.start;
-
-    console.log(state.position.offset, state.position.moved)
-    if(bounded(newPosition, -state.bounds.upper, state.bounds.lower)) {
-      translate(slides, state.position.offset + state.position.moved)
-    }
-
-  });
-}
-
-function ondragend(Carousel, root, state) {
-  listenOn(root, events.end, function(e) {
-     state.dragging = false;
-     state.position.offset =state.position.offset + state.position.moved;
-  });
-}
 
 /**
  * Initialize all drag-based listeners
  */
-function initializeListeners(Carousel) {
-  var root = Carousel.dom.root;
+function listeners() {
+  var self = this;
+  var root = self.root;
+  var slides = self.viewport;
 
-  ondragstart(Carousel, root, Carousel.state);
-  ondragmove(Carousel, root, Carousel.state);
-  ondragend(Carousel, root, Carousel.state);
+  listenOn(root, events.start, function(e) {
+      var pos = coordinates(e);
+
+      /** flag-a-drag, recording the x position of when we started */
+      self.dragging = true;
+      self.eStartPos = pos.x;
+  });
+
+  listenOn(root, events.move, function(e) {
+    var pos = coordinates(e);
+    var distance = pos.x - self.eStartPos
+
+    /** If we're not flagged as dragging, do nothing */
+    if(!self.dragging) return;
+    
+    translate(slides, self.translate + distance);
+  });
+
+  listenOn(root, events.end, function(e) {
+    var pos = coordinates(e);
+    
+    /** unflag-a-drag, and let's record the x position of when we stopped */
+    self.dragging = false;
+    self.eEndPos = pos.x;
+    
+    /** 
+    * We recorded the X position of our start and stop positions because we need to know:
+    * 1. How far we translated (eEndPos - eStartPos)
+    * 2. How much translate was already applied before we even started. 
+
+    * If we translate again later, we'll know what value to begin translating from 
+    */
+    self.translate = self.translate + (self.eEndPos - self.eStartPos);
+
+    console.log(self)
+  });
 }
 
-module.exports = initializeListeners;
+module.exports = listeners;
---
+++ src/js/options/carousel-defaults.js
@@ -0,0 +1,9 @@
+module.exports = {
+  slidesPerView: 2,
+  itemSpacing: 12,
+  classes: {
+    container: '.carousel-container',
+    items: '.carousel-slide',
+    wrapper: '.carousel-wrapper',
+  }
+};
